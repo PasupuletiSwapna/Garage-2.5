c33633abb85b5d50f1effdf3a7dd869209927d0a
pasupuletisaiswapna85 <pasupuletisaiswapna85@gmail.com>
2017-12-19 03:26:23 +0530

    initialcommit

---
+++ Garage2.5/Garage2.5/Models/Member.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Garage2._5.Models
+{
+    public class Member
+    {
+    }
+}
---
+++ Garage2.5/Garage2.5/Models/ParkedVehicle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Garage2._5.Models
+{
+    public class ParkedVehicle
+    {
+
+
+        public int VehicleTypeId { get; set; }
+        public string VType { get; set; }
+
+        public virtual ICollection<ParkedVehicle> Parkedvehicles { get; set; }
+    }
+}
