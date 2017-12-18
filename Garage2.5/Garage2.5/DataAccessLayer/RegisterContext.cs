dadfd79abebf60c31451833151e0c41956785165
pasupuletisaiswapna85 <pasupuletisaiswapna85@gmail.com>
2017-12-19 03:33:17 +0530

    saved

---
+++ Garage2.5/Garage2.5/DataAccessLayer/RegisterContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using 
+
+namespace Garage2._5.DataAccessLayer
+{
+    public class RegisterContext
+    {
+        //public RegisterContext() : base("Garage-2.5")
+        //{ }
+
+        //public DbSet<Models.ParkedVehicle> Vehicles { get; set; }
+        //public DbSet<Models.VehicleType> Vehicletypes { get; set; }
+        //public DbSet<Models.Member> Members { get; set; }
+
+        ////public System.Data.Entity.DbSet<GarageApp_MVC.Models.ViewModels.PrintReceiptView> PrintReceiptViews { get; set; }
+    }
+}
